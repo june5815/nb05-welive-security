0f1657b6fb76c27e07f275e49bdfbf0860af6726
june5815 <dev.junejune@gmail.com>
2026-02-04 14:10:00 +0900

    [fix] ci 테스트확인을 위한 설정 및 수정

--- jest.config.ts
+++ jest.config.ts
@@ -2,5 +2,12 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "\\.skip\\.ts$", "\\.bak\\.ts$"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.skip.ts",
+    "!src/**/*.bak.ts",
+  ],
   verbose: true,
 };
